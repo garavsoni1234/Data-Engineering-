24bcd1f485a4ad991ff71dbbb198224618ea31df
garavsoni1234 <garavsoni12@gmail.com>
2020-07-04 22:52:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+#Project: Data Modeling with Postgres
+
+
+##Prerequisite:
+->Download and Install Python 3.8.3: https://www.python.org/downloads/release/python-383/
+->Add Python to Environment Variables:
+  Step 1: Right click on computer or PC and go to "Properties"
+  Step 2: Click "Environment Variables"
+  Step 3: Under "System Variables" click on "Path"
+  Step 4: Define you Python path by clicking "Add" (Ex: 
+  C:\Users\Garav Soni\AppData\Local\Programs\Python\Python38\Scripts
+  C:\Users\Garav Soni\AppData\Local\Programs\Python\Python38\)
+  Step5: Your Python will now be accessible by IDE
+->Download and install Visual Code Studio: https://code.visualstudio.com/download
+->Download and install PIP: Go to "https://pip.pypa.io/en/stable/installing/" to look at the full documentation on the installation process
+->Use the command to "pip install pycodestyle" to install Pycodestyle which checks for PIP guidelines.
+##How to run:
+
+
+
+
+
+##Summary of Project:
+->This is the first project for the Udacity Data Engineering NanoDegree. In this project, you have to use your knowledge of
+on data modeling with Postgres and build an EFL Pipeline using Python. We have to define face and dimension tables for a star schema. In addition,
+we have ETF pipeline that transfers data from the Song and Log files into Postgres using Python and Postgres
+
+##Steps to do the Project:
+1. The first step to this project is to create the Fact table and dimension tables with the respective properties for each using the "Create" query in the sql_queries.py file.
+2. The next step is to "Insert", so that we can prepare to insert the data from the log data file and song data file in the sql_queries.py file. 
+3. The final step is to create a "SELECT" query to select the accurate song.
+4. After we have finished creating the queries in the sql_queries.py, then we have to go to the 
+
---
+++ sql_queries.py
@@ -0,0 +1,90 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP,
+    user_id INTEGER,
+    level VARCHAR,
+    song_id VARCHAR,
+    artist_id VARCHAR,
+    session_id INTEGER,
+    location VARCHAR,
+    user_agent VARCHAR
+);
+""")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS
+    users(user_id INTEGER  UNIQUE, first_name varchar,
+    last_name varchar, gender char, level varchar)""")
+
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs
+    (song_id varchar UNIQUE, title varchar, artist_id varchar,
+    year integer, duration decimal) """)
+
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
+    (artist_id varchar PRIMARY KEY UNIQUE,
+    name VARCHAR, location VARCHAR, latitude FLOAT,
+    longitude FLOAT)""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time
+    (start_time TIMESTAMP PRIMARY KEY  UNIQUE,
+    hour INTEGER, day INTEGER, week INTEGER, month INTEGER,
+    year INTEGER,weekday INTEGER)""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+    INSERT INTO songplays (songplay_id, start_time, user_id, level,
+    song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT(songplay_id) DO NOTHING;
+    """)
+
+user_table_insert = ("""INSERT INTO users(user_id,
+    first_name,last_name,gender,level)
+    VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO NOTHING""")
+
+song_table_insert = ("""INSERT INTO songs(song_id,title,
+    artist_id,year,duration)
+    VALUES (%s,%s,%s,%s,%s)
+    ON CONFLICT (song_id) DO NOTHING""")
+
+artist_table_insert = ("""INSERT INTO artists(artist_id,name,
+    location,latitude,longitude)
+    VALUES (%s,%s,%s,%s,%s)
+    ON CONFLICT (artist_id) DO NOTHING""")
+
+time_table_insert = (""" INSERT INTO time(start_time, hour, day,
+    week, month, year,weekday)
+    VALUES (%s,%s,%s,%s,%s,%s,%s)
+    ON CONFLICT (start_time) DO NOTHING""")
+
+# FIND SONGS
+
+song_select = ("""
+    SELECT s.song_id, a.artist_id
+            FROM songs AS s
+            LEFT JOIN artists AS a
+                ON a.artist_id = s.artist_id
+            WHERE   s.title = (%s) AND \
+                    a.name = (%s)  AND \
+                    s.duration = (%s);
+""")
+# QUERY LISTS
+
+create_table_queries = [
+                        songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create,
+                        time_table_create]
+drop_table_queries = [
+                        songplay_table_drop, user_table_drop, song_table_drop,
+                        artist_table_drop, time_table_drop]
