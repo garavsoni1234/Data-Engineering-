10f112e0931f582c41e6fcd8923b1daff735ff35
garavsoni1234 <garavsoni12@gmail.com>
2020-07-08 11:18:47 -0400

    update the sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -56,7 +56,7 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 INSERT INTO users(user_id,
     first_name,last_name,gender,level)
-    VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO NOTHING""")
+    VALUES (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level = 1""")
 
 song_table_insert = ("""
 INSERT INTO songs(song_id,title,
